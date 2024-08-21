6268a9992d123b9d644c44a3fd4cf46e352f49df
strwmay <senaidev.may@gmail.com>
2024-08-21 10:36:55 -0300

    Tentativa Trabalho GIT

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Minha Primeira Branch!");
+        Console.WriteLine("Trabalhando com Git/GitHub");
+        Console.WriteLine("Mais uma tentativa de trabalho Git");
     }
 }
